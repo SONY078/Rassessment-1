3454a93fd2b99f2d493328edf6544f64629aed3c
unknown <sony@mine.in>
2022-06-15 21:38:34 +0530

    assignment-1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FunctionC from './assignment-1/FunctionC';
+import ClassC from './assignment-1/ClassC';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Styling using functional and class component</h1>
+      <FunctionC/>
+      <ClassC/>
+    </div>
+  );
+}
+
+export default App;
